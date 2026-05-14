46acf9cf04fbfddb0b75bd5364ab615e9740a12f
Ayushhgi <goyal.ayush2424@gmail.com>
2026-05-14 15:01:41 +0530

    l

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,7 +29,7 @@ app.get('/home', (req, res) => {
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://ecomeet-ed87.onrender.com',
     credentials: true, //allow frontend to send the cookie
   })
 );
