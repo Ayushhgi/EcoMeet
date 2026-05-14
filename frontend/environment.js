961c1f10512b0dbee42795640c865e4146432b38
Ayushhgi <goyal.ayush2424@gmail.com>
2026-05-14 13:00:25 +0530

    add production

--- frontend/environment.js
+++ frontend/environment.js
@@ -1,4 +1,4 @@
-let prodution=false;
-let server=prodution?"https://meethubbackend-imbj.onrender.com":"http://localhost:9002";
+let prodution=true;
+let server=prodution?"https://ecomeet-1yuk.onrender.com":"http://localhost:9002";
 
 export default server;
